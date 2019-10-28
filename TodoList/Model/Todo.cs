7a4a6ec646dd4ad3679da89d5e8a242c5703384f
Jason C. Rana <jason@rananet.local>
2019-10-27 20:41:54 -0500

    Add project files.

---
+++ TodoList/Model/Todo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.ComponentModel;
+
+namespace ToDoList.Model
+{
+  public class Todo
+  {
+    public string item { get; set; }
+    public int id { get; set; }
+    public bool done { get; set; }
+    public int diff { get; set; }
+
+    private string tConn = ConfigurationManager.ConnectionStrings["tConn"].ToString();
+
+    public Todo()
+    {
+      item = "";
+      id = 0;
+      done = false;
+      diff = 0;
+    }
+
+    public void newItem()
+    {
+      using (SqlConnection con = new SqlConnection(tConn))
+      {
+        string q = $"INSERT INTO Todo VALUES ('{item}',0,'{diff}') ";
+        SqlCommand c = new SqlCommand(q, con);
+        con.Open();
+        c.ExecuteNonQuery();
+      }
+    }
+
+    public bool readItem()
+    {
+      using (SqlConnection con = new SqlConnection(tConn))
+      {
+        string q = $"SELECT * FROM Todo WHERE id={id}";
+        SqlCommand c = new SqlCommand(q, con);
+        con.Open();
+        SqlDataReader rows = c.ExecuteReader();
+        if (rows.HasRows) while (rows.Read())
+          {
+            item = rows["item"].ToString();
+            done = (bool)rows["done"];
+            diff = Convert.ToInt32(rows["difficulty"]);
+            return true;
+          }
+      }
+      return false;
+    }
+    public void updateItem()
+    {
+      using (SqlConnection con = new SqlConnection(tConn))
+      {
+        string q = $"UPDATE Todo SET item={item},difficulty={diff} WHERE id={id}";
+        SqlCommand c = new SqlCommand(q, con);
+        con.Open();
+        c.ExecuteNonQuery();
+      }
+    }
+
+    public void completeItem()
+    {
+      using (SqlConnection con = new SqlConnection(tConn))
+      {
+        string q = $"Update Todo SET done=1 WHERE id={id}";
+        SqlCommand c = new SqlCommand(q, con);
+        con.Open();
+        c.ExecuteNonQuery();
+      }
+    }
+
+    public override string ToString()
+    {
+      string t = "<tr><td>";
+      t += "<input type='checkbox' ";
+      t += (done) ? "checked " : "";
+      t += $"id='cb{id}' name='cb{id}' /></td>" +
+        $"<td class='diff{diff}'>{item}</td></tr>\n";
+      return t;
+    }
+  }
+}
